43b94796def70cffeb73f710b3508e02a90f3134
cristinavasiliu <cristina.vasiliu@student.upt.ro>
2022-05-24 14:18:33 +0300

    Finished login/logout design. Jira:SEF-74

--- src/main/java/com/example/sefapplication/LoggedInSceneController.java
+++ src/main/java/com/example/sefapplication/LoggedInSceneController.java
@@ -3,19 +3,32 @@ package com.example.sefapplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class LoggedInSceneController {
+public class LoggedInSceneController implements Initializable {
 
+    @FXML
+    private ImageView image1;
+    @FXML
+    private ImageView image2;
+    @FXML
+    private ImageView image3;
+    @FXML
+    private ImageView image4;
     @FXML
     public Button logoutButton;
     @FXML
@@ -28,6 +41,30 @@ public class LoggedInSceneController {
     private Scene scene;
     @FXML
     private Parent root;
+
+    @Override
+    public void initialize(URL URL, ResourceBundle resourceBundle){
+        File backgroundFile = new File("src/Images/searchButton.png");
+        Image myImage = new Image(backgroundFile.toURI().toString());
+
+        File f1 = new File("src/Images/front_page_template.jpg");
+        Image myImage1 = new Image(f1.toURI().toString());
+
+        File f2 = new File("src/Images/chair3.png");
+        Image myImage2 = new Image(f2.toURI().toString());
+
+        File f3 = new File("src/Images/bed1.jpg");
+        Image myImage3 = new Image(f3.toURI().toString());
+
+        File f4 = new File("src/Images/table1.jpg");
+        Image myImage4 = new Image(f4.toURI().toString());
+
+        searchButton.setImage(myImage);
+        image1.setImage(myImage1);
+        image2.setImage(myImage2);
+        image3.setImage(myImage3);
+        image4.setImage(myImage4);
+    }
 
     public void switchToCartScene(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("CartScene.fxml")));
